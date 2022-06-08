9d0b5e7142d73345215e923222958a2634341c39
Siegfried Dubois <sidubois@muscadier.irisa.fr>
2022-06-08 13:35:22 +0200

    Genouest script

--- genouest_wisp.sh
+++ genouest_wisp.sh
@@ -1,12 +1,27 @@
 #!/bin/sh
+#SBATCH --job-name=wisp
 . /local/env/envconda.sh
 
-conda activate /home/genouest/genscale/sdubois/wisp-env
+conda activate $1
+
+##########################
+#                        #
+#    ARGS YOU CAN USE    #
+#                        #
+##########################
 
 # calling build
 # python wisp.py -b -t 8
 
 # calling dl
-python utilities.py
+# python utilities.py
+
+# calling prediction
+# python wisp.py -t 8
+
+$2
 
 conda deactivate
+
+# example use:
+# sbatch --cpus-per-task=8 --mem=50G genouest_wisp.sh "/home/genouest/genscale/sdubois/wisp-env" "wisp.py -b -t 8"
