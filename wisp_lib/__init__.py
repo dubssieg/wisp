25f470fa486831b266e28ebf2ab117ced6a4a48c
Siegfried Dubois <sidubois@muscadier.irisa.fr>
2022-06-23 10:44:29 +0200

    deletion error

--- wisp_lib/__init__.py
+++ wisp_lib/__init__.py
@@ -12,7 +12,6 @@ from .kmers_coders import encode_kmer_4
 from .kmers_coders import decode_kmer_4
 from .kmers_coders import recode_kmer_4
 from .kmers_coders import kmer_indexing_brut
-from .kmers_coders import my_encoder_k4
 from .kmers_coders import read_and_its_compl
 from .kmers_coders import optimal_splitting
 from .kmers_coders import reverse_comp
