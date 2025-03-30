3143b56e2b89ae13f11e401e5a712ba5eca61fb4
cyrille.leroux@gmail.com <cyrille.leroux@irisa.fr>
2025-03-30 18:30:22 +0200

    simpler generation buffer size

--- wisp_allthebacteria/dataset.py
+++ wisp_allthebacteria/dataset.py
@@ -165,9 +165,7 @@ class ByRankGenerator(Dataset):
         )
         self._rank_tids = list(self._tax_ids_by_rank.keys())
 
-        self._max_buffer_size = min(
-            batch_size, int(max_buffer_total_size / len(self._rank_tids))
-        )
+        self._max_buffer_size = int(max_buffer_total_size / len(self._rank_tids))
 
         self._buffers = {rank_tid: queue.Queue() for rank_tid in self._rank_tids}
         self._exhausted = {rank_tid: False for rank_tid in self._rank_tids}
