6e86b099e97236ac62a8ab9020d45379ba228426
cyrille.leroux@gmail.com <cyrille.leroux@irisa.fr>
2025-04-02 09:26:36 +0200

    fixed dmat_list

--- wisp_allthebacteria/dmatstore.py
+++ wisp_allthebacteria/dmatstore.py
@@ -17,7 +17,7 @@ class DMatStore:
         return self._dmat_path(name).exists()
 
     def dmat_list(self) -> list[str]:
-        return [p.name for p in self._path.rglob(f"*{DMAT_SUFFIX}")]
+        return [p.name for p in self._storage_path().rglob(f"*{DMAT_SUFFIX}")]
 
     def clear(self) -> None:
         for dmat_name in self.dmat_list():
@@ -29,11 +29,14 @@ class DMatStore:
         for name in names:
             yield self._deserialize_dmatrix(name)
 
-    def _dmat_path(self, name: str | int) -> Path:
+    def _storage_path(self) -> Path:
         path = self._path
         if self._signature:
             path /= self._signature
-        return path / f"{name}{DMAT_SUFFIX}"
+        return path
+
+    def _dmat_path(self, name: str | int) -> Path:
+        return self._storage_path() / f"{name}{DMAT_SUFFIX}"
 
     def _serialize_dmatrix(self, dmat: xgb.DMatrix, name: str) -> None:
         dmat_path = self._dmat_path(name)
