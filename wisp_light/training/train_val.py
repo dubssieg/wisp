5ff2a1cd6342087e4d23ffd27d99b193994226ab
COURTEILLE Hermann <hermann.courteille@inria.fr>
2025-04-04 16:24:09 +0200

    fix nthread=1 for xgboost and env var test + optim filtered read

--- wisp_light/training/train_val.py
+++ wisp_light/training/train_val.py
@@ -6,6 +6,9 @@ import yaml
 import logging
 import mlflow
 from datetime import datetime
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
 
 from utils import setup_logger
 from create_database import check_parameters, build_database, load_phylo_tree
@@ -33,10 +36,10 @@ args.index_csv = "../dataset/complete_refseq_referent_genome_with_taxo.tsv"
 
 args.datadir = '/WORKS/microtaxo/data/refseq3' # '/scratch/hcourtei/refseq3'
 args.exp_rootdir = '/WORKS/microtaxo/exp_refseq' # '/scratch/hcourtei/exp_refseq'
+# args.db_json  = "/WORKS/microtaxo/exp_refseq/model_base_testval_04_04_13_48/databases.json"
 
 # args.datadir = '/projects/microtaxo/data/refseq3' # '/scratch/hcourtei/refseq3'
 # args.exp_rootdir = '/projects/microtaxo/exp_refseq' # '/scratch/hcourtei/exp_refseq'
-args.db_json  = "/WORKS/microtaxo/exp_refseq/model_base_testval_04_04_13_48/databases.json"
 
 day_month_min = datetime.now().strftime('%m_%d_%H_%M')
 if args.db_json:
@@ -81,7 +84,7 @@ with mlflow.start_run():
     # dataset = BacteriaDataset(args.datadir, logger)
     # dataset.filter_family_by_min_species(min_family_threshold=params['min_family_threshold'],
     #                                      max_family_repr=params['max_family_repr'])
-    dataset = RefSeqDataset(args.index_csv, args.datadir, logger, cut= -1)
+    dataset = RefSeqDataset(args.index_csv, args.datadir, logger, cut= 1000)
 
     train_dataset, val_dataset = dataset.split(test_size=params['test_size'], random_state=params['random_state'],
                                                family_strat=params['family_strat'])
